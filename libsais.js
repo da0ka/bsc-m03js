bb11b4a228884cfe84886652a5ba102f30a3c0f2
da0ka <gda0ka@gmail.com>
2022-05-28 20:33:13 +0900

    added libsais16_bwt
    supported 16 or more bits symbol. removed several functions

--- libsais.js
+++ libsais.js
@@ -1,7 +1,11 @@
 // based on libsais v1.0.0
 const SUFFIX_GROUP_MARKER=1<<30;
-function SAcopy(A,a,b,n){for(n+=a;a<n;)A[a++]=A[b++]}
+function SAcopy(A,a,b,n){
+	if(n>15)A.set(A.subarray(b,b+n),a);
+	else for(n+=a;a<n;)A[a++]=A[b++]
+}
 function SAmove(A,a,b,n){
+	if(n>15)return SAmove2(A,a,b,n);
 	if(a+n<=b||b+n<=a)for(n+=a;a<n;)A[a++]=A[b++];
 	else{if(a<b){
 		var c=b,C=A.slice(b+b-a,b+n);
@@ -9,6 +13,16 @@ function SAmove(A,a,b,n){
 	}else{C=A.slice(c=a,b+n);
 		for(n+=a;b<c;)A[a++]=A[b++]
 	}for(c=0;a<n;)A[a++]=C[c++]}
+}
+function SAmove2(A,a,b,n){
+	if(a+n<=b||b+n<=a)A.set(A.subarray(b,b+n),a);
+	else if(a<b)
+		n=A.slice(b+b-a,b+n),
+		A.set(A.subarray(b,b+b-a),a),
+		A.set(n,b);
+	else n=A.slice(a,b+n),
+		A.set(A.subarray(b,a),a),
+		A.set(n,a+a-b)
 }
 function libsais_gather_lms_suffixes_8u(T,SA,n){
 	for(var i=n-1,m=i,s=1,c0=T[i],c1=0;i>3;s^1||--m)
@@ -99,7 +113,7 @@ function libsais_initialize_buckets_end_32s_1k(k,B){
 }
 function libsais_initialize_buckets_start_and_end_32s_2k(k,B){
 	for(var i=0,j=0,n=k-1<<1|1;i<n;i+=2)B[j++]=B[i];
-	for(B[j=k]=i=0;--k;)B[++j]=B[i++]
+	B.set(B.subarray(B[k]=0,k-1),k+1)
 }
 function libsais_initialize_buckets_for_lms_suffixes_radix_sort_8u(T,B,fs){
 	for(var s=0,i=T[fs],j=0,C=B.subarray(1024);fs;B[j<<2|s]--)j=i,i=T[--fs],s=s<<1&3|i>j-(1&s);
@@ -154,18 +168,8 @@ function libsais_initialize_buckets_for_partial_sorting_32s_6k(T,k,B,fs,lc){
 	var i=0,j=0,s=lc+1,t=0,C=B.subarray(4*k);
 	for(k=k-1<<2,B[T[fs]<<2|1]++;i<=k;i+=4)C[j]=s,s+=B[i]+B[i+2],t+=B[i+1],B[j++]=s,B[j++]=t
 }
-function libsais_partial_sorting_scan_left_to_right_8u(T,SA,n,B,c,d){
-	var i=0,p=T[--n]<<1|T[n-1]>=T[n],v,x=-1>>>1,iB=B.subarray(1024),dN=B.subarray(512);
-	SA[iB[p]++]=n|1<<31;dN[p]=++d;
-	for(c--;i<c;dN[v]=d)
-		p=SA[i++],d+=p<0,p&=x,SA[iB[v=T[--p]<<1|T[p-1]>=T[p]]++]=p|(dN[v]!=d)<<31,dN[v]=d,
-		p=SA[i++],d+=p<0,p&=x,SA[iB[v=T[--p]<<1|T[p-1]>=T[p]]++]=p|(dN[v]!=d)<<31;
-	for(c++;i<c;dN[v]=d)
-		p=SA[i++],d+=p<0,p&=x,SA[iB[v=T[--p]<<1|T[p-1]>=T[p]]++]=p|(dN[v]!=d)<<31;
-	return d
-}
-function libsais_partial_sorting_scan_left_to_right_32s_6k(T,SA,n,k,B,c,d){
-	var i=0,p=T[--n]<<1|T[n-1]>=T[n],v,x=-1>>>1,iB=B.subarray(4*k),dN=B.subarray(2*k)
+function libsais_partial_sorting_scan_left_to_right_8u(T,SA,n,B,c,d,k){
+	var i=0,p=T[--n]<<1|T[n-1]>=T[n],v,x=-1>>>1,iB=B.subarray(k?4*k:1024),dN=B.subarray(k?2*k:512);
 	SA[iB[p]++]=n|1<<31;dN[p]=++d;
 	for(c--;i<c;dN[v]=d)
 		p=SA[i++],d+=p<0,p&=x,SA[iB[v=T[--p]<<1|T[p-1]>=T[p]]++]=p|(dN[v]!=d)<<31,dN[v]=d,
@@ -187,7 +191,7 @@ function libsais_partial_sorting_scan_left_to_right_32s_4k(T,SA,n,k,B,d){
 			SA[i]=0,d+=p>>30,p&=~SUFFIX_GROUP_MARKER,c=T[--p-1]<T[p],v=T[p]<<1|c,
 			SA[iB[T[p]]++]=p|c<<31|(B[v]!=d)<<30,B[v]=d;
 	}
-	for(j++;i<j;++i){
+	for(;i<n;++i){
 		p=SA[i];SA[i]=p&x;
 		if(p>0)
 			SA[i]=0,d+=p>>30,p&=~SUFFIX_GROUP_MARKER,c=T[--p-1]<T[p],v=T[p]<<1|c,
@@ -199,21 +203,11 @@ function libsais_partial_sorting_scan_left_to_right_32s_1k(T,SA,n,k,B){
 	for(SA[B[T[j]]++]=j|(T[j-1]<T[j])<<31;i<j;++i)
 		p=SA[i],SA[i]=p&x,0<p&&(SA[i]=0,SA[B[T[--p]]++]=p|(T[p-1]<T[p])<<31),
 		p=SA[++i],SA[i]=p&x,0<p&&(SA[i]=0,SA[B[T[--p]]++]=p|(T[p-1]<T[p])<<31);
-	for(j++;i<j;++i)
+	for(;i<n;++i)
 		p=SA[i],SA[i]=p&x,0<p&&(SA[i]=0,SA[B[T[--p]]++]=p|(T[p-1]<T[p])<<31)
 }
-function libsais_partial_sorting_shift_markers_8u(SA,B){
-	for(var C=B.subarray(1024),c=510,p,q,x=1<<31;c>1;){
-		for(var i=C[c],j=B[c-=2]+3,s=x;i>j;)
-			p=SA[--i],s^=q=s^(p&x),SA[i]=p^q,
-			p=SA[--i],s^=q=s^(p&x),SA[i]=p^q,
-			p=SA[--i],s^=q=s^(p&x),SA[i]=p^q,
-			p=SA[--i],s^=q=s^(p&x),SA[i]=p^q;
-		for(j-=3;i>j;SA[i]=p^q)p=SA[--i],s^=q=s^(p&x)
-	}
-}
-function libsais_partial_sorting_shift_markers_32s_6k(SA,k,B){
-	for(var C=B.subarray(4*k),c=k-1<<1,p,q,x=1<<31;c>1;){
+function libsais_partial_sorting_shift_markers_8u(SA,B,k){
+	for(var C=B.subarray(k?4*k:1024),c=k?k-1<<1:510,p,q,x=1<<31;c>1;){
 		for(var i=C[c],j=B[c-=2]+3,s=x;i>j;)
 			p=SA[--i],s^=q=s^(p&x),SA[i]=p^q,
 			p=SA[--i],s^=q=s^(p&x),SA[i]=p^q,
@@ -230,32 +224,24 @@ function libsais_partial_sorting_shift_markers_32s_4k(SA,n){
 		p=SA[--i],s^=q=(p&m^s)&(0<p)<<30,SA[i]=p^q;
 	for(;i;)p=SA[--i],s^=q=(p&m^s)&(0<p)<<30,SA[i]=p^q
 }
-function libsais_partial_sorting_scan_right_to_left_8u(T,SA,n,B,fs,c,d){
-	for(var i=n-fs,j=c+2,p,v,x=-1>>>1,dN=B.subarray(512);i>j;)
+function libsais_partial_sorting_scan_right_to_left_8u(T,SA,n,B,fs,c,d,k){
+	for(var i=n-fs,j=c+2,p,v,x=-1>>>1,dN=B.subarray(2*k||512);i>j;)
 		p=SA[--i],d+=p<0,p&=x,SA[--B[v=T[--p]<<1|T[p-1]>T[p]]]=p|(dN[v]!=d)<<31,dN[v]=d,
 		p=SA[--i],d+=p<0,p&=x,SA[--B[v=T[--p]<<1|T[p-1]>T[p]]]=p|(dN[v]!=d)<<31,dN[v]=d;
 	for(j--;i>j;)
 		p=SA[--i],d+=p<0,p&=x,SA[--B[v=T[--p]<<1|T[p-1]>T[p]]]=p|(dN[v]!=d)<<31,dN[v]=d;
 	return d
 }
-function libsais_partial_sorting_scan_right_to_left_32s_6k(T,SA,n,k,B,fs,c,d){
-	for(var dN=B.subarray(2*k),i=n-fs,j=c+2,p,v,x=-1>>>1;i>j;)
-		p=SA[--i],d+=p<0,p&=x,v=T[--p]<<1|T[p-1]>T[p],SA[--B[v]]=p|(dN[v]!=d)<<31,dN[v]=d,
-		p=SA[--i],d+=p<0,p&=x,v=T[--p]<<1|T[p-1]>T[p],SA[--B[v]]=p|(dN[v]!=d)<<31,dN[v]=d;
-	for(j--;i>j;)
-		p=SA[--i],d+=p<0,p&=x,v=T[--p]<<1|T[p-1]>T[p],SA[--B[v]]=p|(dN[v]!=d)<<31,dN[v]=d;
-	return d
-}
 function libsais_partial_sorting_scan_right_to_left_32s_4k(T,SA,n,k,B,d){
-	for(var iB=B.subarray(3*k),i=n,p,v;i>1;){
+	for(var iB=B.subarray(3*k),i=n,p,v,m=~SUFFIX_GROUP_MARKER;i>1;){
 		p=SA[--i];
-		if(p>0)SA[i]=0,d+=p>>30,p&=~SUFFIX_GROUP_MARKER,v=T[--p]<<1|T[p-1]>T[p],SA[--iB[T[p]]]=p|(v&1)<<31|(B[v]!==d)<<30,B[v]=d;
+		if(p>0)SA[i]=0,d+=p>>30,p&=m,v=T[--p]<<1|T[p-1]>T[p],SA[--iB[T[p]]]=p|(v&1)<<31|(B[v]!==d)<<30,B[v]=d;
 		p=SA[--i];
-		if(p>0)SA[i]=0,d+=p>>30,p&=~SUFFIX_GROUP_MARKER,v=T[--p]<<1|T[p-1]>T[p],SA[--iB[T[p]]]=p|(v&1)<<31|(B[v]!==d)<<30,B[v]=d;
+		if(p>0)SA[i]=0,d+=p>>30,p&=m,v=T[--p]<<1|T[p-1]>T[p],SA[--iB[T[p]]]=p|(v&1)<<31|(B[v]!==d)<<30,B[v]=d;
 	}
 	for(;i;){
 		p=SA[--i];
-		if(p>0)SA[i]=0,d+=p>>30,p&=~SUFFIX_GROUP_MARKER,v=T[--p]<<1|T[p-1]>T[p],SA[--iB[T[p]]]=p|(v&1)<<31|(B[v]!==d)<<30,B[v]=d;
+		if(p>0)SA[i]=0,d+=p>>30,p&=m,v=T[--p]<<1|T[p-1]>T[p],SA[--iB[T[p]]]=p|(v&1)<<31|(B[v]!==d)<<30,B[v]=d;
 	}return d
 }
 function libsais_partial_sorting_scan_right_to_left_32s_1k(T,SA,n,k,B){
@@ -287,11 +273,11 @@ function libsais_induce_partial_order_8u(T,SA,n,B,fs,lc){
 	libsais_partial_sorting_shift_markers_8u(SA,B);
 	libsais_partial_sorting_scan_right_to_left_8u(T,SA,n,B,fs,lc,d)
 }
-function libsais_induce_partial_order_32s_6k(T,SA,n,k,B,fs,lc){
+function libsais_induce_partial_order_32s_6k(T,SA,n,B,fs,lc,k){
 	B.fill(0,2*k,4*k);
-	var d=libsais_partial_sorting_scan_left_to_right_32s_6k(T,SA,n,k,B,lc,0);
-	libsais_partial_sorting_shift_markers_32s_6k(SA,k,B);
-	libsais_partial_sorting_scan_right_to_left_32s_6k(T,SA,n,k,B,fs,lc,d)
+	var d=libsais_partial_sorting_scan_left_to_right_8u(T,SA,n,B,lc,0,k);
+	libsais_partial_sorting_shift_markers_8u(SA,B,k);
+	libsais_partial_sorting_scan_right_to_left_8u(T,SA,n,B,fs,lc,d,k)
 }
 function libsais_induce_partial_order_32s_4k(T,SA,n,k,B){
 	B.fill(0,0,2*k);
@@ -320,14 +306,14 @@ function libsais_renumber_and_gather_lms_suffixes_8u(SA,n,m){
 		p=SA[i++],SAm[(p&x)>>1]=name|o,name+=p<0,
 		p=SA[i++],SAm[(p&x)>>1]=name|o,name+=p<0,
 		p=SA[i++],SAm[(p&x)>>1]=name|o,name+=p<0;
-	for(j+=3;i<j;)p=SA[i++],SAm[(p&x)>>1]=name|o,name+=p<0;
+	for(;i<m;)p=SA[i++],SAm[(p&x)>>1]=name|o,name+=p<0;
 	if(name<m){
 		for(i=m+(n>>1),j=m+3,l=n-1;i>j;)
 			s=SA[--i],SA[l]=s&x,l-=s<0,
 			s=SA[--i],SA[l]=s&x,l-=s<0,
 			s=SA[--i],SA[l]=s&x,l-=s<0,
 			s=SA[--i],SA[l]=s&x,l-=s<0;
-		for(j-=3;i>j;)s=SA[--i],SA[l]=s&x,l-=s<0
+		for(;i>m;)s=SA[--i],SA[l]=s&x,l-=s<0
 	}else for(;m;)SA[--m]&=x;
 	return name
 }
@@ -337,9 +323,9 @@ function libsais_renumber_and_mark_distinct_lms_suffixes_32s_4k(SA,n,m){
 		q=SA[i],SAm[(SA[i++]=q&x)>>1]=name|q&p&o,name+=q<0,
 		r=SA[i],SAm[(SA[i++]=r&x)>>1]=name|r&q&o,name+=r<0,
 		s=SA[i],SAm[(SA[i++]=s&x)>>1]=name|s&r&o,name+=s<0;
-	for(j+=3;i<j;)r=s,s=SA[i],SAm[(SA[i++]=s&x)>>1]=name|s&r&o,name+=s<0;
+	for(;i<m;)r=s,s=SA[i],SAm[(SA[i++]=s&x)>>1]=name|s&r&o,name+=s<0;
 	if(name<=m){
-		for(i=m,j=m+(n>>1)-3,s=-1;i<j;)
+		for(j=m+(n>>1)-3,s=-1;i<j;)
 			p=SA[i],SA[i++]=p&(s|x),p||(p=s),
 			q=SA[i],SA[i++]=q&(p|x),q||(q=p),
 			r=SA[i],SA[i++]=r&(q|x),r||(r=q),
@@ -372,7 +358,7 @@ function libsais_renumber_and_mark_distinct_lms_suffixes_32s_1k(T,SA,n,m){
 		if(pl===ql){for(l=0;T[p+l]===T[q+l]&&++l<pl;);pd=l-pl&o}
 		SAm[q>>1]=name|qd&pd;name+=pd<0;
 	}
-	for(j++;i<j;pd=qd){
+	for(;i<m;pd=qd){
 		q=SA[i++],ql=SAm[q>>1],qd=o;
 		if(pl===ql){for(l=0;T[p+l]===T[q+l]&&++l<pl;);qd=l-pl&o}
 		SAm[p>>1]=name|pd&qd;name+=qd<0;
@@ -380,7 +366,7 @@ function libsais_renumber_and_mark_distinct_lms_suffixes_32s_1k(T,SA,n,m){
 	}
 	SAm[p>>1]=name|pd;
 	if(name<m){
-		for(i=m,j=m+(n>>1)-3,l=-1;i<j;)
+		for(j=m+(n>>1)-3,l=-1;i<j;)
 			p=SA[i],SA[i++]=p&(l|x),p||(p=l),
 			q=SA[i],SA[i++]=q&(p|x),q||(q=p),
 			m=SA[i],SA[i++]=m&(q|x),m||(m=q),
@@ -391,7 +377,7 @@ function libsais_renumber_and_mark_distinct_lms_suffixes_32s_1k(T,SA,n,m){
 function libsais_reconstruct_lms_suffixes(SA,n,m){
 	for(var SAnm=SA.subarray(n-m),i=0,j=m-3;i<j;)
 		SA[i]=SAnm[SA[i++]],SA[i]=SAnm[SA[i++]],SA[i]=SAnm[SA[i++]],SA[i]=SAnm[SA[i++]];
-	for(j+=3;i<j;)SA[i]=SAnm[SA[i++]]
+	for(;i<m;)SA[i]=SAnm[SA[i++]]
 }
 function libsais_place_lms_suffixes_interval_8u(SA,n,m,B){
 	for(var E=B.subarray(1792),c=255,i,j=n,l;c--;){
@@ -451,28 +437,18 @@ function libsais_final_bwt_scan_left_to_right_8u(T,SA,n,B){
 		p=SA[i];SA[i]=p&x;if(p>0)SA[i]=T[--p]|o,SA[iB[T[p]]++]=p|(T[p-(0<p)]<T[p])<<31;
 		p=SA[++i];SA[i]=p&x;if(p>0)SA[i]=T[--p]|o,SA[iB[T[p]]++]=p|(T[p-(0<p)]<T[p])<<31
 	}
-	for(j++;i<j;++i){
+	for(;i<n;++i){
 		p=SA[i];SA[i]=p&x;if(p>0)SA[i]=T[--p]|o,SA[iB[T[p]]++]=p|(T[p-(0<p)]<T[p])<<31
-	}
-}
-function libsais_final_sorting_scan_left_to_right_8u(T,SA,n,B){
-	var i=0,j=n-1,o=1<<31,p,iB=B.subarray(1536);
-	for(SA[iB[T[j]]++]=j|(T[j-1]<T[j])<<31;i<j;++i){
-		p=SA[i];SA[i]=p^o;if(p>0)SA[iB[T[--p]]++]=p|(T[p-(0<p)]<T[p])<<31;
-		p=SA[++i];SA[i]=p^o;if(p>0)SA[iB[T[--p]]++]=p|(T[p-(0<p)]<T[p])<<31
-	}
-	for(j++;i<j;++i){
-		p=SA[i];SA[i]=p^o;if(p>0)SA[iB[T[--p]]++]=p|(T[p-(0<p)]<T[p])<<31
 	}
 }
 function libsais_final_sorting_scan_left_to_right_32s(T,SA,n,B){
 	var i=0,j=n-1,o=1<<31,p;
-	for(SA[B[T[j]]++]=j|(T[j-1]<T[j])<<31;i<j;++i){
-		p=SA[i];SA[i]=p^o;if(p>0)SA[B[T[--p]]++]=p|(T[p-(0<p)]<T[p])<<31;
-		p=SA[++i];SA[i]=p^o;if(p>0)SA[B[T[--p]]++]=p|(T[p-(0<p)]<T[p])<<31
+	for(SA[B[T[j]]++]=j|(T[j-1]<T[j])<<31;i<j;){
+		p=SA[i];SA[i++]=p^o;if(p>0)SA[B[T[--p]]++]=p|(T[p-(0<p)]<T[p])<<31;
+		p=SA[i];SA[i++]=p^o;if(p>0)SA[B[T[--p]]++]=p|(T[p-(0<p)]<T[p])<<31
 	}
-	for(j++;i<j;++i){
-		p=SA[i];SA[i]=p^o;if(p>0)SA[B[T[--p]]++]=p|(T[p-(0<p)]<T[p])<<31
+	for(;i<n;){
+		p=SA[i];SA[i++]=p^o;if(p>0)SA[B[T[--p]]++]=p|(T[p-(0<p)]<T[p])<<31
 	}
 }
 function libsais_final_bwt_scan_right_to_left_8u(T,SA,n,B){
@@ -486,15 +462,6 @@ function libsais_final_bwt_scan_right_to_left_8u(T,SA,n,B){
 		p=SA[--i];p||(n=i);SA[i]=p&x;
 		if(p>0)c=SA[i]=T[--p],b=T[p-(p>0)],SA[--iB[c]]=b<=c?p:b|o
 	}return n
-}
-function libsais_final_sorting_scan_right_to_left_8u(T,SA,n,B){
-	for(var i=n,p,x=-1>>>1,iB=B.subarray(1792);i>1;){
-		p=SA[--i];SA[i]=p&x;if(p>0)SA[--iB[T[--p]]]=p|(T[p-(0<p)]>T[p])<<31;
-		p=SA[--i];SA[i]=p&x;if(p>0)SA[--iB[T[--p]]]=p|(T[p-(0<p)]>T[p])<<31
-	}
-	for(;i;){
-		p=SA[--i];SA[i]=p&x;if(p>0)SA[--iB[T[--p]]]=p|(T[p-(0<p)]>T[p])<<31
-	}
 }
 function libsais_final_sorting_scan_right_to_left_32s(T,SA,n,iB){
 	for(var i=n,p,x=-1>>>1;i>1;){
@@ -510,8 +477,8 @@ function libsais_induce_final_order_8u(T,SA,n,bwt,B){
 		libsais_final_bwt_scan_left_to_right_8u(T,SA,n,B);
 		return libsais_final_bwt_scan_right_to_left_8u(T,SA,n,B)
 	}
-	libsais_final_sorting_scan_left_to_right_8u(T,SA,n,B);
-	libsais_final_sorting_scan_right_to_left_8u(T,SA,n,B);
+	libsais_final_sorting_scan_left_to_right_32s(T,SA,n,B.subarray(1536));
+	libsais_final_sorting_scan_right_to_left_32s(T,SA,n,B.subarray(1792));
 	return 0
 }
 function libsais_induce_final_order_32s_6k(T,SA,n,k,B){
@@ -541,7 +508,7 @@ function libsais_compact_lms_suffixes_32s(T,SA,n,m,fs){
 		p=SA[i++];s=SAm[p>>>1];if(s<0)T[p]|=o,s=i+o+f++;SAm[p>>>1]=s-f;
 		p=SA[i++];s=SAm[p>>>1];if(s<0)T[p]|=o,s=i+o+f++;SAm[p>>>1]=s-f
 	}
-	for(j+=3;i<j;){
+	for(;i<m;){
 		p=SA[i++];s=SAm[p>>>1];if(s<0)T[p]|=o,s=i+o+f++;SAm[p>>>1]=s-f
 	}
 	for(i=m+(n>>1),j=m+3;i>j;)
@@ -549,7 +516,7 @@ function libsais_compact_lms_suffixes_32s(T,SA,n,m,fs){
 		p=SA[--i],SA[l]=p&x,l-=p<0,SA[r]=p-1,r-=p>0,
 		p=SA[--i],SA[l]=p&x,l-=p<0,SA[r]=p-1,r-=p>0,
 		p=SA[--i],SA[l]=p&x,l-=p<0,SA[r]=p-1,r-=p>0;
-	for(j-=3;i>j;)p=SA[--i],SA[l]=p&x,l-=p<0,SA[r]=p-1,r-=p>0;
+	for(;i>m;)p=SA[--i],SA[l]=p&x,l-=p<0,SA[r]=p-1,r-=p>0;
 	SAcopy(SA,n+fs-m,l+1,f);
 	return f
 }
@@ -560,7 +527,7 @@ function libsais_merge_compacted_lms_suffixes_32s(T,SA,n,m,f){
 		c=T[++i];if(c<0)T[i]=c&x,SA[s]=i++,s=SAnm[++l];
 		c=T[++i];if(c<0)T[i]=c&x,SA[s]=i++,s=SAnm[++l]
 	}
-	for(j+=6;i<j;++i){
+	for(;i<n;++i){
 		c=T[i];if(c<0)T[i]=c&x,SA[s]=i++,s=SAnm[++l]
 	}
 	for(i=0,j=m-3,s=SAnm[l=f];i<j;++i)
@@ -568,7 +535,7 @@ function libsais_merge_compacted_lms_suffixes_32s(T,SA,n,m,f){
 		SA[++i]||(SA[i]=s,s=SAnm[++l]),
 		SA[++i]||(SA[i]=s,s=SAnm[++l]),
 		SA[++i]||(SA[i]=s,s=SAnm[++l]);
-	for(j+=3;i<j;++i)SA[i]||(SA[i]=s,s=SAnm[++l])
+	for(;i<m;++i)SA[i]||(SA[i]=s,s=SAnm[++l])
 }
 function libsais_reconstruct_compacted_lms_suffixes_32s_2k(T,SA,n,k,m,fs,f,B){
 	if(f>0)
@@ -604,7 +571,7 @@ function libsais_main_32s(T,SA,n,k,fs){
 			libsais_radix_sort_lms_suffixes_32s_2k(T,SA,n,m,B.subarray(4*k));
 			libsais_radix_sort_set_markers_32s(SA,k,B.subarray(4*k),1<<31);
 			libsais_initialize_buckets_for_partial_sorting_32s_6k(T,k,B,f,c);
-			libsais_induce_partial_order_32s_6k(T,SA,n,k,B,f,c);
+			libsais_induce_partial_order_32s_6k(T,SA,n,B,f,c,k);
 			var names=libsais_renumber_and_mark_distinct_lms_suffixes_32s_4k(SA,n,m);
 			if(names<m){
 				f=libsais_compact_lms_suffixes_32s(T,SA,n,m,fs);
@@ -676,11 +643,11 @@ function libsais_main_32s(T,SA,n,k,fs){
 		libsais_induce_partial_order_32s_1k(T,SA,n,k,B);
 		names=libsais_renumber_and_mark_distinct_lms_suffixes_32s_1k(T,SA,n,m);
 		if(names<m){
-			if(A)B=null;
+			if(A)A=B=null;
 			f=libsais_compact_lms_suffixes_32s(T,SA,n,m,fs);
 			if(libsais_main_32s(SA.subarray(n+fs-m+f),SA,m-f,names-f,fs+n-2*m+f))return-2;
 			libsais_reconstruct_compacted_lms_suffixes_32s_1k(T,SA,n,k,m,fs,f);
-			if(!B)B=A=new Int32Array(k+4096)
+			if(!B)B=new Int32Array(k+4096)
 		}
 		libsais_count_suffixes_32s(T,n,k,B);
 		libsais_initialize_buckets_end_32s_1k(k,B);
@@ -715,6 +682,7 @@ function libsais(T,SA,n){
 	if(n<2){if(n)SA[0]=0;return 0}
 	return libsais_main_8u(T,SA,n,0)
 }
+// BWT for 8bits symbol
 function libsais_bwt(T,U,A,C,n){
 	if(!T||!U)return-1;
 	n=n||T.length;A=A||new Int32Array(n);
@@ -726,4 +694,16 @@ function libsais_bwt(T,U,A,C,n){
 		libsais_bwt_copy_8u(U.subarray(1),A,C,i++),
 		libsais_bwt_copy_8u(U.subarray(i),A.subarray(i),C,n-i);
 	return i
+}
+// BWT for 16bits symbol. @T must be Int32Array
+function libsais16_bwt(T,U,A,C,n,k){
+	if(!T||!U)return-1;k>>>=0;
+	n=n||T.length;A=A||new Int32Array(n);
+	C=C||new Int32Array(k+1);
+	if(n<2){if(n)C[U[0]=T[0]]=1;return n}
+	libsais_main_32s(T,A,n,k||1<<16,0);
+	var i=0,s,p;
+	for(C[U[0]=T[n-1]]=1;s=A[i++];)C[U[i]=T[--s]]++;
+	for(p=i;i<n;)C[U[i]=T[A[i++]-1]]++;
+	return p
 }
